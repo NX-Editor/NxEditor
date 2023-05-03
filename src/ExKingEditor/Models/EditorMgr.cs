bd7c79211af3aff4fbd985b81d8fdae5cb3958bb
ArchLeaders <archparliament@gmail.com>
2023-05-03 02:19:23 -0700

    Impl EditorMgr

---
+++ src/ExKingEditor/Models/EditorMgr.cs
@@ -0,0 +1,35 @@
+﻿using ExKingEditor.Core.Extensions;
+
+namespace ExKingEditor.Models;
+
+public static class EditorMgr
+{
+    private static readonly Dictionary<string, string> _vms = EmbedExtension.Parse<App, Dictionary<string, string>>("Supported.json")!;
+
+    public static ReactiveEditor GetEditor(string file)
+    {
+        string ext = GetExt(file);
+        object? instance = Activator.CreateInstance(Type.GetType($"{nameof(ExKingEditor)}.ViewModels.Editors.{_vms[ext]}")
+            ?? throw new InvalidDataException($"Invalid editor type for '{ext}' - '{_vms[ext]}' was not found"), file);
+
+        if (instance is ReactiveEditor editor) {
+            return editor;
+        }
+
+        throw new InvalidCastException($"Could not cast the found type of {instance} to {nameof(ReactiveEditor)}");
+    }
+
+    public static bool CanEdit(string file)
+    {
+        return _vms.ContainsKey(GetExt(file));
+    }
+
+    private static string GetExt(string file)
+    {
+        if (file.EndsWith(".zs")) {
+            file = file[..^3];
+        }
+
+        return file[file.IndexOf('.')..];
+    }
+}
