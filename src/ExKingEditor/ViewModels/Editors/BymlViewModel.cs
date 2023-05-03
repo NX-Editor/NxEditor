f64c36cf5a4ea5b089cf7181ee247f5d923774d5
ArchLeaders <archparliament@gmail.com>
2023-05-03 00:30:31 -0700

    Create Byml Editor View/ViewModel

---
+++ src/ExKingEditor/ViewModels/Editors/BymlViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ExKingEditor.Models;
+
+namespace ExKingEditor.ViewModels.Editors;
+
+public partial class BymlViewModel : ReactiveEditor
+{
+    public readonly string _yaml;
+    public string Yaml { get; set; }
+
+    public BymlViewModel(string file) : base(file)
+    {
+        // Load source into readonly field for diffing
+        _yaml = """
+            Yaml: key
+            Array:
+             - a: 1
+             - b: 2
+             - c: 3
+             - d: true
+            """;
+
+        Yaml = _yaml;
+    }
+
+    public override void SaveEditor()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool HasChanged()
+    {
+        return _yaml != Yaml;
+    }
+}
---
+++ src/ExKingEditor/Views/Editors/BymlView.axaml.cs
@@ -0,0 +1,33 @@
+using Avalonia.Controls;
+using AvaloniaEdit;
+using AvaloniaEdit.TextMate;
+using ExKingEditor.ViewModels.Editors;
+using TextMateSharp.Grammars;
+
+namespace ExKingEditor.Views.Editors;
+
+public partial class BymlView : UserControl
+{
+    private static readonly RegistryOptions registryOptions = new(ThemeName.DarkPlus);
+
+    public BymlView()
+    {
+        InitializeComponent();
+
+        // Initialize TextEditor
+        TextMate.Installation textMateInstallation = TextEditor.InstallTextMate(registryOptions);
+        textMateInstallation.SetGrammar("source.yaml");
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+
+        if (DataContext is BymlViewModel vm) {
+            TextEditor.Text = vm.Yaml;
+            TextEditor.TextChanged += (s, e) => {
+                vm.Yaml = TextEditor.Text;
+            };
+        }
+    }
+}
