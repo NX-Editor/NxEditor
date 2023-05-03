81c8aa64586933e7b783cb24872bbcd7a4e09116
ArchLeaders <archparliament@gmail.com>
2023-05-02 20:53:42 -0700

    Impl reactive editor - auto decompress - abstract load/save methods

---
+++ src/ExKingEditor.Core/TotkZstd.cs
@@ -0,0 +1,47 @@
+﻿using ZstdSharp;
+
+namespace ExKingEditor.Core;
+
+public class TotkZstd
+{
+    private static readonly string _zsDicPath = Path.Combine(TotkConfig.Shared.GamePath, "Pack", "ZsDic.pack.zs");
+
+    private static readonly Decompressor _commonDecompressor = new();
+    private static readonly Decompressor _mapDecompressor = new();
+    private static readonly Decompressor _packDecompressor = new();
+
+    private static readonly Compressor _commonCompressor = new(15);
+    private static readonly Compressor _mapCompressor = new(15);
+    private static readonly Compressor _packCompressor = new(15);
+
+    static TotkZstd()
+    {
+        Span<byte> data = _commonDecompressor.Unwrap(File.ReadAllBytes(_zsDicPath));
+        using Sarc sarc = Sarc.FromBinary(data);
+
+        _commonDecompressor.LoadDictionary(sarc["zs.zsdic"]);
+        _mapDecompressor.LoadDictionary(sarc["bcett.byml.zsdic"]);
+        _packDecompressor.LoadDictionary(sarc["pack.zsdic"]);
+
+        _commonCompressor.LoadDictionary(sarc["zs.zsdic"]);
+        _mapCompressor.LoadDictionary(sarc["bcett.byml.zsdic"]);
+        _packCompressor.LoadDictionary(sarc["pack.zsdic"]);
+    }
+
+    public static Span<byte> Decompress(string file)
+    {
+        Span<byte> src = File.ReadAllBytes(file);
+        return
+            file.EndsWith(".bcett.byml.zs") ? _mapDecompressor.Unwrap(src) :
+            file.EndsWith(".pack.zs") ? _packDecompressor.Unwrap(src) :
+            _commonDecompressor.Unwrap(src);
+    }
+    public static Span<byte> Compress(string file)
+    {
+        Span<byte> src = File.ReadAllBytes(file);
+        return
+            file.EndsWith(".bcett.byml") ? _mapCompressor.Wrap(src) :
+            file.EndsWith(".pack") ? _packCompressor.Wrap(src) :
+            _commonCompressor.Wrap(src);
+    }
+}
---
+++ src/ExKingEditor/Models/ReactiveEditor.cs
@@ -0,0 +1,35 @@
+﻿using ExKingEditor.Core;
+
+namespace ExKingEditor.Models;
+
+public abstract unsafe class ReactiveEditor : ReactiveObject
+{
+    protected readonly string _file;
+    protected readonly bool _compressed;
+
+    private readonly byte* _data;
+    private readonly int _length;
+
+    public ReactiveEditor(string file)
+    {
+        _file = file;
+        _compressed = file.EndsWith(".zs");
+
+        Span<byte> data = TotkZstd.Decompress(file);
+        _length = data.Length;
+
+        // Store a ptr on the heap to
+        // avoid copying to an array
+        fixed (byte* ptr = data) {
+            _data = ptr;
+        }
+    }
+
+    public abstract ReactiveEditor LoadEditor();
+    public abstract void SaveEditor();
+
+    protected Span<byte> RawData()
+    {
+        return new(_data, _length);
+    }
+}
