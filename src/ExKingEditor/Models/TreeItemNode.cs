86253f1f004fcb834035377f4ebed9026f0e5584
ArchLeaders <archparliament@gmail.com>
2023-05-05 21:43:34 -0700

    Create generic tree item node

---
+++ src/ExKingEditor/Models/TreeItemNode.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace ExKingEditor.Models;
+
+public partial class TreeItemNode : ObservableObject
+{
+    [ObservableProperty]
+    private string _header = string.Empty;
+
+    [ObservableProperty]
+    private ObservableCollection<TreeItemNode> _children = new();
+
+    public TreeItemNode(string header)
+    {
+        _header = header;
+    }
+}
