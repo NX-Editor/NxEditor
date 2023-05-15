3bced075b0384d6aaa50df0a2578fd83fed5a7eb
ArchLeaders <archparliament@gmail.com>
2023-05-15 09:37:20 -0700

    Impl single instance app manager

--- src/ExKingEditor/Program.cs
+++ src/ExKingEditor/Program.cs
@@ -1,6 +1,9 @@
 using Avalonia;
 using Avalonia.ReactiveUI;
+using Avalonia.Threading;
+using ExKingEditor.Core.Utils;
 using ExKingEditor.Generators.UI;
+using ExKingEditor.Models;
 using Projektanker.Icons.Avalonia;
 using Projektanker.Icons.Avalonia.FontAwesome;
 
@@ -12,8 +15,20 @@ internal class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static unsafe void Main(string[] args)
+    {
+        if (SingleInstanceManager.Start(args, Attach)) {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+    }
+
+    public static void Attach(string[] args)
+    {
+        Dispatcher.UIThread.InvokeAsync(() => {
+            App.Desktop?.Activate();
+            EditorMgr.TryLoadEditorSafe(args[0]);
+        });
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
