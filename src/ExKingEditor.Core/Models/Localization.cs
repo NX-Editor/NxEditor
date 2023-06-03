caca94ef821e39463c41cb55e22794db6aee19ce
ArchLeaders <archparliament@gmail.com>
2023-06-03 11:42:22 -0700

    Push localization stubs

---
+++ src/ExKingEditor.Core/Models/Localization.cs
@@ -0,0 +1,21 @@
+﻿using ExKingEditor.Core.Exceptions;
+using ExKingEditor.Core.Extensions;
+using System.Reflection;
+
+namespace ExKingEditor.Core.Models;
+
+public class Localization
+{
+    public static Localization Texts { get; set; } = null!;
+    public static Dictionary<string, string> TextsMap { get; set; } = null!;
+
+    public static void Load(string localizationName = "USen")
+    {
+        Assembly asm = typeof(Localization).Assembly;
+        Texts = asm.ParseEmbed<Localization>($"Localizations/{localizationName}.json")
+            ?? throw new LocalizationException($"Unsupported localization: '{localizationName}'");
+        TextsMap = asm.ParseEmbed<Dictionary<string, string>>($"Localizations/{localizationName}.json")!;
+    }
+
+    public required string ShellMenu_File_OpenFile { get; set; }
+}
