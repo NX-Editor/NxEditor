6818a110630bb7098a9e7759b34074ba109e3430
ArchLeaders <archparliament@gmail.com>
2023-06-13 14:49:26 -0700

    Define ITransformable interface

---
+++ src/NxEditor.Plugin/Generics/ITransformable.cs
@@ -0,0 +1,18 @@
+﻿namespace NxEditor.Plugin.Generics;
+
+public interface ITransformable
+{
+    /// <summary>
+    /// Transforms the raw data and returns the result
+    /// </summary>
+    /// <param name="raw"></param>
+    /// <returns></returns>
+    public Span<byte> Transform(Span<byte> raw);
+
+    /// <summary>
+    /// Runs the reverse transform and returns the result
+    /// </summary>
+    /// <param name="transformed"></param>
+    /// <returns></returns>
+    public Span<byte> FromTransformed(Span<byte> transformed);
+}
