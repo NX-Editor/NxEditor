637061f8acd8fbd4d8131ebb8aa80770c1f6423f
ArchLeaders <archparliament@gmail.com>
2023-06-13 15:16:58 -0700

    Add CommandAttribute

---
+++ src/NxEditor.Plugin/Attributes/CommandAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace NxEditor.Plugin.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class CommandAttribute : Attribute
+{
+    /// <summary>
+    /// The name used in the CLI to call this command (should not contain spaces)
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// The description of this command displayed in the help command
+    /// </summary>
+    public required string Description { get; set; }
+}
