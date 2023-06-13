bb99d0de2b03679052a607db08f777d93e82b06a
ArchLeaders <archparliament@gmail.com>
2023-06-12 20:43:43 -0700

    Impl double click to edit RestblEntry

--- src/NxEditor/Views/Editors/RestblView.axaml.cs
+++ src/NxEditor/Views/Editors/RestblView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using NxEditor.ViewModels.Editors;
 
 namespace NxEditor.Views.Editors;
 public partial class RestblView : UserControl
@@ -6,5 +8,12 @@ public partial class RestblView : UserControl
     public RestblView()
     {
         InitializeComponent();
+    }
+
+    public void EditEvent(object sender, TappedEventArgs e)
+    {
+        if (DataContext is RestblViewModel vm) {
+            vm.Current?.Edit(vm);
+        }
     }
 }
