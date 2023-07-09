fff7852d10438dd753f00156116b471b703dbe63
ArchLeaders <archparliament@gmail.com>
2023-07-09 00:39:10 -0700

    Use ObservableProperties in Config class

--- src/NxEditor.Core/Config.cs
+++ src/NxEditor.Core/Config.cs
@@ -1,42 +1,54 @@
-﻿using ConfigFactory.Core;
+﻿using Avalonia;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ConfigFactory.Core;
 using ConfigFactory.Core.Attributes;
 
 namespace NxEditor.Core;
 
-public class Config : ConfigModule<Config>
+public partial class Config : ConfigModule<Config>
 {
     public override string Name => "nx-editor";
     public static string AppFolder { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "nx-editor");
 
-    [Config(
+    [ObservableProperty]
+    [property: Config(
         Header = "Load Resources from Disk",
         Description = "Loads a customizable copy of the resource files from disk instead of donwloading the latest version from the live server")]
-    public bool LoadResourcesFromDisk { get; set; }
+    private bool _loadResourcesFromDisk;
 
-    [Config(
+    [ObservableProperty]
+    [property: Config(
         Header = "Single Instance Lock",
         Description = "Files opened through the shell will be opened as a tab in the current instance, rather than starting a second running application",
-        Category = "Lock Settings")]
-    public bool UseSingleInstance { get; set; } = true;
+        Group = "Lock Settings")]
+    private bool _useSingleInstance = true;
 
-    [Config(
+    [ObservableProperty]
+    [property: Config(
         Header = "Single File Lock",
         Description = "Restrict two files with the same path to be loaded concurrently",
-        Category = "Lock Settings")]
-    public bool UseSingleFileLock { get; set; } = false;
+        Group = "Lock Settings")]
+    private bool _useSingleFileLock = false;
 
-    [Config(
+    [ObservableProperty]
+    [property: Config(
         Header = "Default RESTBL Hash Table",
         Description = "The absolue path to the hash table loaded when a RESTBL file is opened\n\n(Please paste the file path in, file browsing is currently not supported)",
-        Category = "RESTBL",
-        Group = "Editor Config")]
-    [BrowserConfig(
+        Group = "RESTBL",
+        Category = "Editor Config")]
+    [property: BrowserConfig(
         BrowserMode = BrowserMode.OpenFile,
         Title = "Browse for a RESTBl string table")]
-    public string DefaultHashTable { get; set; } = string.Empty;
+    private string _defaultHashTable = string.Empty;
 
-    [Config(Header = "Theme", Description = "", Category = "Appearance")]
-    public string Theme { get; set; } = "Dark";
+    [ObservableProperty]
+    [property: Config(
+        Header = "Theme",
+        Description = "",
+        Group = "Appearance")]
+    [property: DropdownConfig("Dark", "Light")]
+    private string _theme = "Dark";
 
     public static bool ValidatePath(string path, string key)
     {
@@ -48,5 +60,17 @@ public class Config : ConfigModule<Config>
             "GamePath" => File.Exists(Path.Combine(path, "Pack", "ZsDic.pack.zs")),
             _ => false,
         };
+    }
+
+    partial void OnThemeChanged(string? oldValue, string newValue)
+    {
+        SetTheme(newValue);
+    }
+
+    public static void SetTheme(string variant)
+    {
+        if (Application.Current is Application app) {
+            app.RequestedThemeVariant = variant == "Dark" ? ThemeVariant.Dark : ThemeVariant.Light;
+        }
     }
 }
