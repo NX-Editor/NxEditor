9df4bc8210e8f5e9ec7b11e4182f4a79b5cdda8f
ArchLeaders <archleadership28@gmail.com>
2024-04-13 11:15:37 -0700

    Code cleanup

--- src/NxEditor/Program.cs
+++ src/NxEditor/Program.cs
@@ -9,9 +9,6 @@ namespace NxEditor;
 
 internal class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things
-    // aren't initialized yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
@@ -54,7 +51,6 @@ internal class Program
         });
     }
 
-    // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
     {
         IconProvider.Current.Register(new FontAwesomeIconProvider());
