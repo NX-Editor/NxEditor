34b1f62a4425cdd65e5f856c65531ea16217abfa
ArchLeaders <archleadership28@gmail.com>
2024-06-02 00:38:31 -0700

    Create core static class (NXE)

---
+++ src/NxEditor.Core/NXE.cs
@@ -0,0 +1,12 @@
+﻿namespace NxEditor.Core;
+
+public static class NXE
+{
+    static NXE()
+    {
+        Directory.CreateDirectory(SystemPath);
+    }
+
+    public static readonly string SystemPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".nxe");
+    public static readonly AppConfig Config = new();
+}
