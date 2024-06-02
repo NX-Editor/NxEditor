5389ca5d88295f729c663c86cd827021538cf343
ArchLeaders <archleadership28@gmail.com>
2024-06-02 00:37:23 -0700

    Create AppConfig

---
+++ src/NxEditor.Core/AppConfig.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Globalization;
+using System.Text.Json;
+
+namespace NxEditor.Core;
+
+public enum AppTheme
+{
+    System,
+    Dark,
+    Light,
+}
+
+public partial class AppConfig : ObservableValidator
+{
+    private static readonly string _appConfigFilePath = Path.Combine(NXE.SystemPath, "AppConfig.json");
+
+    public event EventHandler<AppTheme>? AppThemeChanged;
+
+    [ObservableProperty]
+    private AppTheme _theme = AppTheme.System;
+
+    [ObservableProperty]
+    private string _cultureName = CultureInfo.CurrentCulture.TextInfo.CultureName;
+
+    public void Save()
+    {
+        using FileStream fs = File.Create(_appConfigFilePath);
+        JsonSerializer.Serialize(fs, this);
+    }
+
+    internal static AppConfig LoadFromDisk()
+    {
+        if (!File.Exists(_appConfigFilePath)) {
+            return new();
+        }
+
+        using FileStream fs = File.OpenRead(_appConfigFilePath);
+        return JsonSerializer.Deserialize<AppConfig>(fs)
+            ?? new();
+    }
+
+    partial void OnThemeChanged(AppTheme value)
+    {
+        AppThemeChanged?.Invoke(this, value);
+    }
+}
