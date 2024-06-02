98f29fe54e5f3c885bb3a31c35675f45cbdb2a11
ArchLeaders <archleadership28@gmail.com>
2024-06-01 22:52:56 -0700

    Update icon

--- src/NxEditor/Views/ShellView.axaml.cs
+++ src/NxEditor/Views/ShellView.axaml.cs
@@ -1,3 +1,5 @@
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using FluentAvalonia.UI.Windowing;
 
 namespace NxEditor.Views;
@@ -7,5 +9,10 @@ public partial class ShellView : AppWindow
     public ShellView()
     {
         InitializeComponent();
+
+        TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
+
+        Bitmap bitmap = new(AssetLoader.Open(new Uri("avares://NxEditor/Assets/Icon.ico")));
+        Icon = bitmap.CreateScaledBitmap(new(48, 48), BitmapInterpolationMode.HighQuality);
     }
 }
