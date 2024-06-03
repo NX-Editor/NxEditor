c8a2d549057a79d7afe197e264446104d0474d16
ArchLeaders <archleadership28@gmail.com>
2024-06-02 23:11:10 -0700

    Fix hit test height

--- src/NxEditor/Views/ShellView.axaml.cs
+++ src/NxEditor/Views/ShellView.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 using FluentAvalonia.UI.Windowing;
@@ -10,8 +11,11 @@ public partial class ShellView : AppWindow
     {
         InitializeComponent();
 
-        TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
         TitleBar.ExtendsContentIntoTitleBar = true;
+        TitleBar.TitleBarHitTestType = TitleBarHitTestType.Simple;
+        TitleBar.SetDragRectangles([
+            new Rect(0, 0, double.PositiveInfinity, 35)
+        ]);
 
         const string AVARES_ICON_PATH = "avares://NxEditor/Assets/Icon.ico";
         Bitmap bitmap = new(AssetLoader.Open(new Uri(AVARES_ICON_PATH)));
