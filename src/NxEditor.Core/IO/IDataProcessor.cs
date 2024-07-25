6788e672227dce589e99397ae9e5fa7359ca7b41
ArchLeaders <archleadership28@gmail.com>
2024-07-25 12:36:27 -0700

    Impl DataProcessorService

---
+++ src/NxEditor.Core/IO/IDataProcessor.cs
@@ -0,0 +1,25 @@
+﻿using NxEditor.Core.Models;
+
+namespace NxEditor.Core.IO;
+
+public interface IDataProcessor
+{
+    /// <summary>
+    /// Remove any processing applied to the <paramref name="payload"/>.
+    /// </summary>
+    /// <param name="payload"></param>
+    void RemoveProcessing(FilePayload payload);
+
+    /// <summary>
+    /// Apply processing to the <paramref name="payload"/>.
+    /// </summary>
+    /// <param name="payload"></param>
+    void ApplyProcessing(FilePayload payload);
+
+    /// <summary>
+    /// Determines if the <paramref name="payload"/> is processed by this <see cref="IDataProcessor"/>
+    /// </summary>
+    /// <param name="payload"></param>
+    /// <returns><see langword="true"/> if the <paramref name="payload"/> is processed by this <see cref="IDataProcessor"/></returns>
+    bool IsProcessingApplied(FilePayload payload);
+}
---
+++ src/NxEditor.Core/IO/Services/DataProcessorService.cs
@@ -0,0 +1,50 @@
+﻿using NxEditor.Core.Models;
+
+namespace NxEditor.Core.IO.Services;
+
+public class DataProcessorService
+{
+    private static readonly List<IDataProcessor> _dataProcessors = [];
+
+    /// <summary>
+    /// Add a new <see cref="IDataProcessor"/> to the collection of data processors.
+    /// </summary>
+    /// <param name="dataProcessor"></param>
+    public static void Register(IDataProcessor dataProcessor)
+    {
+        _dataProcessors.Add(dataProcessor);
+    }
+
+    /// <summary>
+    /// Remove all processing from the provided <paramref name="payload"/>.
+    /// </summary>
+    /// <param name="payload"></param>
+    /// <param name="appliedDataProcessors">The list of processors applied to the <paramref name="payload"/></param>
+    public static void RemoveProcessing(FilePayload payload, out List<IDataProcessor> appliedDataProcessors)
+    {
+        appliedDataProcessors = [];
+
+        foreach (IDataProcessor processor in GetDataProcessors(payload)) {
+            processor.RemoveProcessing(payload);
+            appliedDataProcessors.Add(processor);
+        }
+
+        appliedDataProcessors.Reverse();
+    }
+
+    /// <summary>
+    /// Recursively retrieve all processors applied to the <paramref name="payload"/> until all processing is removed or no processors can be found.
+    /// </summary>
+    /// <param name="payload"></param>
+    /// <returns></returns>
+    public static IEnumerable<IDataProcessor> GetDataProcessors(FilePayload payload)
+    {
+    ResetLoop:
+        foreach (IDataProcessor processor in _dataProcessors) {
+            if (processor.IsProcessingApplied(payload)) {
+                yield return processor;
+                goto ResetLoop;
+            }
+        }
+    }
+}
