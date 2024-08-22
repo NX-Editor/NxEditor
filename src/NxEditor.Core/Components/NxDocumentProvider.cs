fc769485eacab12dbf95a1047288e6027ac97a2c
ArchLeaders <archleadership28@gmail.com>
2024-08-21 22:03:42 -0700

    Impl DocumentProvider as abstract class

---
+++ src/NxEditor.Core/Components/NxDocumentProvider.cs
@@ -0,0 +1,19 @@
+﻿using NxEditor.Core.Models;
+
+namespace NxEditor.Core.Components;
+
+public abstract class NxDocumentProvider
+{
+    public abstract string Name { get; }
+    public abstract ValueTask<bool> CanRead(FilePayload filePayload);
+    public abstract ValueTask<INxDocument> GetDocument(FilePayload filePayload);
+    public async ValueTask<T?> GetDocument<T>(FilePayload filePayload) where T : class, INxDocument
+    {
+        return await GetDocument(filePayload) as T;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
--- src/NxEditor.Core/Components/Services/NxDocumentManager.cs
+++ src/NxEditor.Core/Components/Services/NxDocumentManager.cs
@@ -4,22 +4,22 @@ namespace NxEditor.Core.Components.Services;
 
 public static class NxDocumentManager
 {
-    private static readonly List<INxDocumentProvider> _documentProviders = [];
+    private static readonly List<NxDocumentProvider> _documentProviders = [];
 
     /// <summary>
-    /// Add a new <see cref="INxDocumentProvider"/> to the collection of document providers.
+    /// Add a new <see cref="NxDocumentProvider"/> to the collection of document providers.
     /// </summary>
     /// <param name="documentProvider"></param>
-    public static void Register(INxDocumentProvider documentProvider)
+    public static void Register(NxDocumentProvider documentProvider)
     {
         _documentProviders.Add(documentProvider);
     }
 
     /// <summary>
-    /// Returns a collection of valid <see cref="INxDocumentProvider"/>'s for the provided <paramref name="filePayload"/>.
+    /// Returns a collection of valid <see cref="NxDocumentProvider"/>'s for the provided <paramref name="filePayload"/>.
     /// </summary>
     /// <param name="filePayload"></param>
-    public static async IAsyncEnumerable<INxDocumentProvider> GetProviders(FilePayload filePayload)
+    public static async IAsyncEnumerable<NxDocumentProvider> GetProviders(FilePayload filePayload)
     {
         foreach (var provider in _documentProviders) {
             if (await provider.CanRead(filePayload)) {
