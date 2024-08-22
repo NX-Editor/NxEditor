841f1904e38b9f5ec842198d213b4892495c2495
ArchLeaders <archleadership28@gmail.com>
2024-08-21 22:00:48 -0700

    Impl NXE.OpenFile

--- src/NxEditor.Core/NXE.cs
+++ src/NxEditor.Core/NXE.cs
@@ -1,4 +1,9 @@
-﻿namespace NxEditor.Core;
+﻿using NxEditor.Core.Components;
+using NxEditor.Core.Components.Services;
+using NxEditor.Core.IO;
+using NxEditor.Core.Models;
+
+namespace NxEditor.Core;
 
 public static class NXE
 {
@@ -11,4 +16,23 @@ public static class NXE
     public static readonly NxeConfig Config = new();
     public static readonly NxeStatus Status = new();
     public static readonly NxeLogger Logger = new();
+
+    public static async ValueTask<T?> OpenFile<T>(string filePath) where T : class, INxDocument
+    {
+        return await OpenFile(filePath) as T;
+    }
+
+    public static async ValueTask<INxDocument?> OpenFile(string filePath)
+    {
+        IDataProvider dataProvider = new StreamDataProvider(File.OpenRead(filePath));
+        FilePayload payload = new(filePath, dataProvider);
+
+        NxProcessorManager.RemoveProcessing(payload);
+
+        await foreach (var provider in NxDocumentManager.GetProviders(payload)) {
+            return await provider.GetDocument(payload);
+        }
+
+        return null;
+    }
 }
