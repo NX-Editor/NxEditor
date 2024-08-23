b1d7049f50c13643a1b8d4f1536a623a5467b5c0
ArchLeaders <archleadership28@gmail.com>
2024-08-22 23:07:26 -0700

    Create document editor interface

---
+++ src/NxEditor.Core/Components/INxDocumentEditor.cs
@@ -0,0 +1,11 @@
+﻿namespace NxEditor.Core.Components;
+
+public interface INxDocumentEditor
+{
+    ValueTask Undo();
+    ValueTask Redo();
+    ValueTask SelectAll();
+    ValueTask Cut();
+    ValueTask Copy();
+    ValueTask Paste();
+}
